dff9348411c7379d8ef9105729f9f38d32bbf179
gulshat1705 <gulshatarkytbekova@gmail.com>
2023-04-23 21:58:08 +0600

    feat: add uniqe class for numbers and letters

--- index.js
+++ index.js
@@ -1,3 +1,166 @@
+let keys = [
+    {
+        id: 1,
+        itemClass: 'backquote',
+        content: '`'
+    },
+    {
+        id: 2,
+        itemClass: null,
+        content: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']  
+    },
+    {
+        id: 3,
+        itemClass: 'minus',
+        content: '-', 
+    },
+    {
+        id: 4,
+        itemClass: 'equal',
+        content: '=', 
+    },    
+    {
+        id: 5,
+        itemClass: 'backspace',
+        content: 'Backspace', 
+    },
+    {
+        id: 6,
+        itemClass: 'tab',
+        content: 'Tab', 
+    },
+    {
+        id: 7,
+        itemClass: null,
+        content: ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"], 
+    },
+    {
+        id: 8,
+        itemClass: 'bracket-left',
+        content: '[', 
+    },
+    {
+        id: 9,
+        itemClass: 'bracket-right',
+        content: ']', 
+    },
+    {
+        id: 10,
+        itemClass: 'backslash',
+        content: '\\', 
+    },
+    {
+        id: 11,
+        itemClass: 'delete',
+        content: 'Del', 
+    },
+    {
+        id: 12,
+        itemClass: 'capslock',
+        content: 'CapsLock', 
+    },
+    {
+        id: 13,
+        itemClass: null,
+        content: ["a", "s", "d", "f", "g", "h", "j", "k", "l"], 
+    },
+    {
+        id: 14,
+        itemClass: 'semicolon',
+        content: ';', 
+    },
+    {
+        id: 15,
+        itemClass: 'quote',
+        content: '\'', 
+    },
+    {
+        id: 16,
+        itemClass: 'enter',
+        content: 'Enter', 
+    },
+    {
+        id: 17,
+        itemClass: 'shift-left',
+        content: 'Shift', 
+    },
+    {
+        id: 18,
+        itemClass: null,
+        content: ["z", "x", "c", "v", "b", "n", "m"], 
+    },
+    {
+        id: 19,
+        itemClass: 'comma',
+        content: ',', 
+    },
+    {
+        id: 20,
+        itemClass: 'period',
+        content: '.', 
+    },
+    {
+        id: 21,
+        itemClass: 'slash',
+        content: '/', 
+    },
+    {
+        id: 22,
+        itemClass: 'arrow-up',
+        content: '▲', 
+    },
+    {
+        id: 23,
+        itemClass: 'shift-right',
+        content: 'Shift', 
+    },
+    {
+        id: 24,
+        itemClass: 'control-left',
+        content: 'Ctrl', 
+    },
+    {
+        id: 25,
+        itemClass: 'window',
+        content: 'Win', 
+    },
+    {
+        id: 26,
+        itemClass: 'alt',
+        content: 'Alt', 
+    },
+    {
+        id: 27,
+        itemClass: 'Space',
+        content: ' ', 
+    },
+    {
+        id: 28,
+        itemClass: 'alt-right',
+        content: 'Alt', 
+    },
+    {
+        id: 29,
+        itemClass: 'arrow-left',
+        content: '◄', 
+    },
+    {
+        id: 30,
+        itemClass: 'arrow-down',
+        content: '▼', 
+    },
+    {
+        id: 31,
+        itemClass: 'arrow-right',
+        content: '►', 
+    },
+    {
+        id: 32,
+        itemClass: 'control-right',
+        content: 'Ctrl', 
+    }  
+];
+console.log(keys[6].content[2]);
 const body = document.querySelector('body');
 const wrapper = document.createElement('div');
 const title = document.createElement('h1');
@@ -54,40 +217,81 @@ keyboard__btn.forEach(function(item){
 })
 
 const en = document.querySelectorAll('.en');
-function createKeys() {    
-
-    let keyContent = [
-       "`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace",
-       "Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\", "Del",
-       "CapsLock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter",
-       "Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "&#708;", "Shift",
-       "Ctrl", "Win", "Alt", "Space", "Alt", "&#706;", "&#709;", "&#707;", "Ctrl"   
-    ];
-
-    for(let i=0; i<keyContent.length; i++) {
-        let checkNaN = NaN;
-        en[i].textContent = keyContent[i];
-        if(typeof Number(keyContent[i]) === 'number' && Number(keyContent[i])) {
-            console.log(Number(keyContent[i]));
-            keyboard__btn[i].classList.add('number'+keyContent[i]);
-        } else {
-            keyboard__btn[i].classList.add('Key'+keyContent[i]);
-        }
+let contentValue = (keys) => {
+    let contentArray = [];
+    for (let item of keys) {
+        contentArray.push(item.content);
     }
-    return keyContent;  
+    return contentArray.flat();
+} 
+
+let result = contentValue(keys);
+
+
+
+let classValue = (keys) => {
+    let ids = keys.map(function (item) {
+        return parseInt(item.content, 10);
+      });
+
+      let classArray = [];
+    for (let item of keys) {
+        if(item.itemClass === null && Number(item.content) === 'number') {
+            for (let i =0; i<contentIsNum.length; i++) {
+                item.className = 'number'+i;
+            }
+        }
+        classArray.push(item.itemClass);
+    }
+    return classArray.flat();
+} 
+
+let addClass = classValue(keys);
+
+for(let i=0; i<en.length; i++) {
+    en[i].textContent = result[i];
+        
+    if(Number(result[i])) {
+        en[i].className = 'number'+i;
+        if(result[i]==='0') {
+            en[i].className = 'number0';
+        } 
+
+    } else if(Boolean(result[i].match(/^[A-Za-z]*$/) && result[i].length === 1)) {
+        en[i].classList.add('letter'+result[i].toUpperCase());
+    } else {
+        // en[i].classList.add(keys.itemClass)
+    }    
 }
 
-createKeys();
 
-window.addEventListener(
-    "keydown",
-    (event) => {
-        console.log(`KeyboardEvent: key='${event.key}' | code='${event.code}'`)
+
+// window.addEventListener(
+//     "keydown",
+//     (event) => {
+
+//         for(let i=0; i<en.length;i++) {
+//             en[i].textContent = event.key;
+//         }
+
+//         console.log(`KeyboardEvent: key='${event.key}' | code='${event.code}'`)
+//         console.log(key);
       
-    //   textarea.textContent = `KeyboardEvent: key='${event.key}' | code='${event.code}'`;
+//     //   textarea.textContent = `KeyboardEvent: key='${event.key}' | code='${event.code}'`;
 
-    //   document.getElementById("output").appendChild(p);
-      window.scrollTo(0, document.body.scrollHeight);
-    },
-    true
-  );
+//     //   document.getElementById("output").appendChild(p);
+//       window.scrollTo(0, document.body.scrollHeight);
+//     },
+//     true
+//   );
+
+  //Function
+function keyboardEvents(e) {
+    e.preventDefault();
+    textarea.textContent += e.key;
+    // keyCodeEvent.textContent = e.keyCode;
+    // CodeEvent.textContent = e.code;
+    // whichEvent.textContent = e.which;
+    }
+    // Event Handler
+    window.addEventListener("keydown", keyboardEvents);
