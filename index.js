260b2ba25a435b8372149bd8604c6b6b332a8ecd
gulshat1705 <arkytbekovagulshat@gmail.com>
2023-05-02 12:19:09 +0600

    feat: add textarea click

--- index.js
+++ index.js
@@ -534,12 +534,14 @@ window.addEventListener('keyup', (e) => {
 })
 
 for(let i=0; i<keyboard__btn.length; i++) {
-    keyboard__btn[i].addEventListener('click', (e) => {
+    keyboard__btn[i].addEventListener('click', () => {
       keyboard__btn[i].classList.add('active-key');
     //   textarea.textContent += en[i].textContent;  
     if(keyboard__btn[i].textContent.length === 1 || keyboard__btn[i].textContent.length === '▲') {
-        textarea.textContent += e.target.textContent;
-    }     
+        console.log(textarea)
+        textarea.value += keyboard__btn[i].textContent;
+    } 
+    textarea.focus()  
       keyboard__btn[i].classList.remove('active-key');
       
     })
